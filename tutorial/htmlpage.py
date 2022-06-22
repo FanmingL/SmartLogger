b83d16f145c56cbdca0548a5bbecb8d49fce20c0
luofm <luofm@lamda.nju.edu.cn>
2022-06-22 10:44:51 +0800

    add argparse for htmlpage

--- tutorial/htmlpage.py
+++ tutorial/htmlpage.py
@@ -4,13 +4,26 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import smart_logger.common.page_config as page_config
 import smart_logger.common.plot_config as plot_config
 from smart_logger.front_page.page import start_page_server
-
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description=f'数据可视化服务启动参数配置')
+
+    parser.add_argument('--workspace_path', '-wks', type=str, default='~/Desktop/small_logger_cache',
+                        help="Path to the workspace, used to saving cache data")
+    parser.add_argument('--data_path', '-d', type=str, default='/full/path/to/data',
+                        help="Path to the data")
+    parser.add_argument('--user_name', '-u', type=str, default='user',
+                        help="user name")
+    parser.add_argument('--password', '-pw', type=str, default='123456',
+                        help="password")
+    parser.add_argument('--port', '-p', type=int, default=7005,
+                        help="Server port")
+    args = parser.parse_args()
     # 关键配置项1：数据目录，该目录下存有日志文件
-    plot_config.DATA_PATH = '/full/path/to/data'
+    plot_config.DATA_PATH = args.data_path
     # 关键配置项2：本地工作目录，该目录会自动创建，并会在里面写入一些缓存文件，如缓存的配置信息，绘制的曲线图
-    page_config.WORKSPAPCE = os.path.expanduser('~/Desktop/small_logger_cache')
+    page_config.WORKSPAPCE = os.path.expanduser(args.workspace_path)
 
     # 如何判断哪些日志会被看作是同一个算法
     plot_config.DATA_MERGER = []
@@ -27,7 +40,12 @@ def main():
     page_config.CONFIG_PATH = f"{page_config.WEB_RAM_PATH}/configs"
     # 图像绘制的保存目录
     page_config.FIGURE_PATH = f"{page_config.WEB_RAM_PATH}/figures"
-
+    # 端口
+    page_config.PORT = args.port
+    # 账号
+    page_config.USER_NAME = args.user_name
+    # 密码
+    page_config.PASSWD = args.password
     start_page_server()
 
 
